b523a50055539020379fc90f3c5882b588b3fd91
user148189 <user148189@74-80-245-25.ic2net.net>
2019-04-08 21:17:57 -0700

    First Draft

--- TouchExplorer/ViewController.swift
+++ TouchExplorer/ViewController.swift
@@ -2,19 +2,56 @@
 //  ViewController.swift
 //  TouchExplorer
 //
-//  Created by user148189 on 4/8/19.
+//  Created by esanchez on 4/8/19.
 //  Copyright © 2019 Tec. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var tapsLabel: UILabel!
+    @IBOutlet var touchesLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-}
+    private func updateLabelsFromTouches(_ touch: UITouch?, allTouches: Set<UITouch>?) {
+        let numTaps = touch?.tapCount ?? 0
+        let tapsMessage = "\(numTaps) taps detected"
+        tapsLabel.text = tapsMessage
+        
+        let numTouches = allTouches?.count ?? 0
+        let touchMsg = "\(numTouches) touches detected"
+        touchesLabel.text = touchMsg
+        
+        if traitCollection.forceTouchCapability == .available {
+            forceLabel.text = "Force: \(touch?.force ?? 0)\nMax force: \(touch?.maximumPossibleForce ?? 0)"
+        } else {
+            forceLabel.text = "3D Touch not available"
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Began"
+        updateLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Cancelled"
+        updateLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Ended"
+        updateLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Drag Detected"
+        updateLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }}
 
